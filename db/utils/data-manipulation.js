364ed7fbc7cc9fb3ab04a8c75063319765588ba0
williamyap90 <william.yap90@gmail.com>
2021-08-02 20:12:47 +0100

    Added formatCommentsData function to format commentData.

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,9 +1,3 @@
-// exports.formatData = (userData) => {
-//   return userData.map((user) => {
-//     return [user.username, user.name, user.avatar_url];
-//   });
-// };
-
 exports.formatData = (data) => {
   let result = [];
 
@@ -18,10 +12,36 @@ exports.formatData = (data) => {
   return result;
 };
 
-// exports.formatCategoryData = (categoryData) => {
-//   return categoryData.map((cat) => {
-//     return [cat.slug, cat.description];
-//   });
-// };
+exports.createCommentsRefObj = (data) => {
+  const commentsRefObj = {};
 
-exports.formatReviewData = (reviewData) => {};
+  data.forEach((review) => {
+    commentsRefObj[review["title"]] = review["review_id"];
+  });
+
+  return commentsRefObj;
+};
+
+exports.formatCommentsData = (data, commentsRefObj) => {
+  const result = data.map((comment) => {
+    const newComment = { ...comment };
+
+    let commentsTitle = newComment.belongs_to;
+
+    for (let key in commentsRefObj) {
+      if (key === commentsTitle) {
+        newComment["review_id"] = commentsRefObj[key];
+        delete newComment.belongs_to;
+      }
+    }
+
+    return [
+      newComment.created_by,
+      newComment.review_id,
+      newComment.votes,
+      newComment.created_at.toISOString(),
+      newComment.body,
+    ];
+  });
+  return result;
+};
