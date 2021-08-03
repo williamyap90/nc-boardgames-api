b3caed966911aaf2769f605c351ba1cf5c338643
williamyap90 <william.yap90@gmail.com>
2021-08-03 19:36:15 +0100

    Added additional error handling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { handleRouter404s } = require("./errors");
+const { handleRouter404s, handle500s, handlePSQLErrors } = require("./errors");
 const apiRouter = require("./routers/api.router");
 
 const app = express();
@@ -9,5 +9,7 @@ app.use(express.json());
 app.use("/api", apiRouter);
 
 app.use(handleRouter404s);
+app.use(handlePSQLErrors);
+app.use(handle500s);
 
 module.exports = app;
--- errors.js
+++ errors.js
@@ -1,3 +1,14 @@
 exports.handleRouter404s = (req, res, next) => {
-  res.status(404).send({ msg: "invalid path" });
+  res.status(404).send({ message: "Invalid path" });
+};
+
+exports.handlePSQLErrors = (err, req, res, net) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Invalid review_id" });
+  }
+};
+
+exports.handle500s = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: "Internal server error" });
 };
