d5224361e5cdd7b6742e9025af24d9bb89da657c
williamyap90 <william.yap90@gmail.com>
2021-08-03 15:18:02 +0100

    Added testing for api endpoint & categories.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,29 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app.js");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("/api", () => {
+  test("GET - status 200, response from the api endpoint", async () => {
+    const res = await request(app).get("/api").expect(200);
+    expect(res.body.msg).toBe("All ok from /api");
+  });
+});
+
+describe("/api/notapath", () => {
+  test("404 - returns custom error message", async () => {});
+});
+
+describe("/api/categories", () => {
+  describe("GET", () => {
+    test("200, returns a array of categories", async () => {
+      const res = await request(app).get("/api/categories").expect(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body).toHaveLength(4);
+    });
+  });
+});
