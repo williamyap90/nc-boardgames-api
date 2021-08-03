15e809a545cbb1add87f3985f32059a3c92c917e
williamyap90 <william.yap90@gmail.com>
2021-08-03 19:37:33 +0100

    Added catch block to catch err

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -2,7 +2,11 @@ const { fetchReviewById } = require("../models/reviews.model");
 
 exports.getReviewById = (req, res, next) => {
   const { review_id } = req.params;
-  fetchReviewById(review_id).then((result) => {
-    res.status(200).send(result);
-  });
+  fetchReviewById(review_id)
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
