6a967bedbf63a56b51d0d39f542235ccb5d68b7f
williamyap90 <william.yap90@gmail.com>
2021-08-04 09:17:35 +0100

    Refactored code for invalid review ids & valid but non-existent ids

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -1,15 +1,23 @@
 const db = require("../db/connection");
 
 exports.fetchReviewById = async (review_id) => {
-  const result = await db.query(
-    `
-    SELECT reviews.owner, title, reviews.review_id, review_body, designer, review_img_url, category, reviews.created_at, reviews.votes, COUNT(comment_id) AS comment_count
-    FROM reviews 
-    JOIN comments 
-    ON reviews.review_id = comments.review_id 
-    WHERE reviews.review_id=$1
-    GROUP BY reviews.owner, reviews.title, reviews.review_id;`,
-    [review_id]
-  );
-  return result.rows;
+  const queryValue = [review_id];
+  let queryString = `
+        SELECT reviews.owner, title, reviews.review_id, review_body, designer, review_img_url, category, reviews.created_at, reviews.votes, COUNT(comment_id) AS comment_count
+        FROM reviews 
+        JOIN comments 
+        ON reviews.review_id = comments.review_id 
+        WHERE reviews.review_id=$1
+        GROUP BY reviews.owner, reviews.title, reviews.review_id;
+    `;
+
+  const { rows } = await db.query(queryString, queryValue);
+
+  if (rows.length === 0) {
+    return Promise.reject({
+      status: 404,
+      message: `Review id ${review_id} not found`,
+    });
+  }
+  return rows;
 };
