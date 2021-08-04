986da380e9807f27668027ba0795d82036fe7ff4
williamyap90 <william.yap90@gmail.com>
2021-08-04 16:03:24 +0100

    Added get endpoint for /api/reviews/

--- routers/reviews.router.js
+++ routers/reviews.router.js
@@ -2,8 +2,10 @@ const reviewsRouter = require("express").Router();
 const {
   getReviewById,
   updateReviewById,
+  getReviews,
 } = require("../controllers/reviews.controller");
 
+reviewsRouter.get("/", getReviews);
 reviewsRouter.route("/:review_id").get(getReviewById).patch(updateReviewById);
 
 module.exports = reviewsRouter;
