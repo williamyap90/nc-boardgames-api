e9924204962cc232e229813257cb205decad007d
williamyap90 <william.yap90@gmail.com>
2021-08-04 16:56:58 +0100

    Created getReviews controller

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -1,4 +1,8 @@
-const { fetchReviewById, patchReviewById } = require("../models/reviews.model");
+const {
+  fetchReviewById,
+  patchReviewById,
+  fetchReviews,
+} = require("../models/reviews.model");
 
 exports.getReviewById = (req, res, next) => {
   const { review_id } = req.params;
@@ -19,6 +23,17 @@ exports.updateReviewById = (req, res, next) => {
   patchReviewById({ updateBody, review_id })
     .then((review) => {
       res.status(201).send(review);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getReviews = (req, res, next) => {
+  const query = req.query;
+  fetchReviews(query)
+    .then((reviews) => {
+      res.status(200).send(reviews);
     })
     .catch((err) => {
       next(err);
