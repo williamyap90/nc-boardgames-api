feed75774a391a8f8d40d484900de77bc8989ab1
williamyap90 <william.yap90@gmail.com>
2021-08-05 11:07:45 +0100

    added production database config

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,23 @@
-const { Pool } = require('pg');
-const path = require('path');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const path = require("path");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: path.resolve(__dirname, `../.env.${ENV}`),
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+module.exports = new Pool(config);
