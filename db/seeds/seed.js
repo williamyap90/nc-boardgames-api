f90513cb1a23427bdf8364fe647578eedb7e72b3
williamyap90 <william.yap90@gmail.com>
2021-08-05 12:50:59 +0100

    modified not null constraints on tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -47,7 +47,7 @@ const seed = async (data) => {
       comment_id SERIAL PRIMARY KEY,
       author VARCHAR(100) REFERENCES users(username) ON DELETE CASCADE NOT NULL,
       review_id INT REFERENCES reviews(review_id) ON DELETE CASCADE NOT NULL,
-      votes INT DEFAULT 0 NOT NULL,
+      votes INT DEFAULT 0,
       created_at TIMESTAMP DEFAULT NOW(),
       body VARCHAR(1000) NOT NULL
     );
