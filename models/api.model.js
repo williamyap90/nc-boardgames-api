7d3f8721c9289594648d171dfaae4647cd84dcf8
williamyap90 <william.yap90@gmail.com>
2021-08-05 16:19:49 +0100

    modified result from fs.readFile JSON object to display in correct format on heroku

--- models/api.model.js
+++ models/api.model.js
@@ -2,6 +2,6 @@ const db = require("../db/connection");
 const fs = require("fs/promises");
 
 exports.fetchEndpoints = async () => {
-  const result = await fs.readFile("endpoints.json", "utf8");
-  return result;
+  const result = await fs.readFile("endpoints.json");
+  return JSON.parse(result.toString());
 };
