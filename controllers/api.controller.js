f3fc00c4df39ade0337846a0de827126fcc5d5b9
williamyap90 <william.yap90@gmail.com>
2021-08-05 16:13:20 +0100

    removed console.log

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,5 +1,7 @@
 const { fetchEndpoints } = require("../models/api.model");
 
 exports.getEndpoints = (req, res, next) => {
-  res.status(200).send({ message: "All ok from /api" });
+  fetchEndpoints().then((endpoints) => {
+    res.status(200).send({ endpoints });
+  });
 };
