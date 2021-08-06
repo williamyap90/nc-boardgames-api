b4ed9d348c3a7814c31ad5422375e39e94884ca9
williamyap90 <william.yap90@gmail.com>
2021-08-06 06:28:54 +0100

    added catch block to deleteCommentById

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -2,7 +2,11 @@ const { removeCommentById } = require("../models/comments.model");
 
 exports.deleteCommentById = (req, res, next) => {
   const { comment_id } = req.params;
-  removeCommentById({ comment_id }).then((review) => {
-    res.status(204).send(review);
-  });
+  removeCommentById({ comment_id })
+    .then((review) => {
+      res.status(204).send(review);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
