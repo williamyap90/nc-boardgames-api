beac3fd1035d195585d33bfbb16eb56b78590116
williamyap90 <william.yap90@gmail.com>
2021-08-06 13:01:36 +0100

    added users router, controller & endpoint

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,22 @@
-const { fetchUsers } = require("../models/users.model");
+const { fetchUsers, fetchUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
-  fetchUsers().then((users) => {
-    res.status(200).send({ users });
-  });
+  fetchUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  fetchUserByUsername({ username })
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
--- models/users.model.js
+++ models/users.model.js
@@ -1,6 +1,26 @@
 const db = require("../db/connection");
+const { checkExists } = require("../helpers");
 
 exports.fetchUsers = async () => {
   const result = await db.query(`SELECT * FROM users;`);
+  return result.rows;
+};
+
+exports.fetchUserByUsername = async ({ username }) => {
+  const usernameExists = await checkExists("users", "username", username);
+  if (!usernameExists) {
+    return Promise.reject({
+      status: 404,
+      message: `Username "${username}" does not exist`,
+    });
+  }
+
+  const queryString = `
+      SELECT * FROM users
+      WHERE username = $1
+    ;`;
+  const queryValues = [username];
+
+  const result = await db.query(queryString, queryValues);
   return result.rows;
 };
