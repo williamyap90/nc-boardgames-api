c060085b7804040c3ba8b16de016b0f87e1cd9a9
williamyap90 <william.yap90@gmail.com>
2021-08-08 10:58:12 +0100

    add missing catch block to getEndpoints

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,7 +1,11 @@
 const { fetchEndpoints } = require("../models/api.model");
 
 exports.getEndpoints = (req, res, next) => {
-  fetchEndpoints().then((endpoints) => {
-    res.status(200).send({ endpoints });
-  });
+  fetchEndpoints()
+    .then((endpoints) => {
+      res.status(200).send({ endpoints });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
