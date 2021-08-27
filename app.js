8f0666c4aae0fb446ce740ce01d735ae4881fe4d
williamyap90 <william.yap90@gmail.com>
2021-08-27 11:24:55 +0100

    enable cors

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const {
   handlePSQLErrors,
   handleCustomErrors,
 } = require("./errors");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 
